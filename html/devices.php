3a96cead83675b6d37346b6a3bf575aa7494086b
DevxMike <163881@stud.prz.edu.pl>
2023-06-12 00:31:22 +0000

    changing working parameters of the device implemented

--- html/devices.php
+++ html/devices.php
@@ -16,6 +16,7 @@ if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
   <style>
     .font_size{
 	font-size: 16px;
@@ -89,12 +90,129 @@ if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
 <div class="container-fluid text-center">  
   <div class="row content">
     <div class="col-sm-12 text-left"> 
-      <h1>Device</h1>
+        <div class="container mt-5">
+    <form id="settings">
+      <div class="form-group">
+        <label for="input1">Device ID:</label>
+        <input type="number" class="form-control" id="input1" name="input1">
+      </div>
+      <div class="form-group">
+        <label for="input2">High threshold:</label>
+        <input type="number" class="form-control" id="input2" name="input2">
+      </div>
+      <div class="form-group">
+        <label for="input3">Low threshold:</label>
+        <input type="number" class="form-control" id="input3" name="input3">
+      </div>
+      <div class="form-group">
+        <label for="input4">Set/Clear alarm:</label>
+        <select class="form-control" id="input4" name="input4">
+          <option value="0">Alarm cleared</option>
+          <option value="1">Alarm set</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="input5">Logging frequency:</label>
+        <select class="form-control" id="input5" name="input5">
+          <option value="0">Every 10 minutes</option>
+          <option value="1">Every 30 minutes</option>
+          <option value="2">Every hour</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="input6">Chart update frequency:</label>
+        <select class="form-control" id="input6" name="input6">
+          <option value="0">Every 10 minutes</option>
+          <option value="1">Every 30 minutes</option>
+          <option value="2">Every hour</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
       <!-- <p>Our venture offers many IoT devices such as weather stations, home alarm systems and so on.</p> -->
     </div>
     </div>
   </div>
 </div>
+<script>
+
+function generateRandomString(length) {
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  
+  return result;
+}
+ // Connect to MQTT broker
+      var client = new Paho.MQTT.Client("159.65.116.172", 9001, generateRandomString(10));
+      var options = {
+        timeout: 3,
+	onSuccess: onConnect
+      };
+      client.onConnectionLost = onDisconnect;
+
+      client.connect({
+        ... options
+      });
+	function onDisconnect(responseObject) {
+	  if (responseObject.errorCode !== 0) {
+	    console.log("Connection lost: " + responseObject.errorMessage);
+	    client.connect({ ... options});
+	  }
+	}
+      function sendMessage(topic, msg) {
+        var payload = new Paho.MQTT.Message(msg);
+        payload.destinationName = topic;
+        client.send(payload);
+      }
+
+      // Subscribe to topic
+      function onConnect() {
+        console.log("Connected to MQTT broker.");
+	    client.subscribe("ws_init/dev_settings");
+      }
+      
+      // Display received messages
+      client.onMessageArrived = function(message) {
+    }
+
+document.getElementById("settings").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  console.log("Hello");
+
+  var dev_id = parseInt(document.getElementById("input1").value);
+  var hi_tr = parseInt(document.getElementById("input2").value);
+  var lo_tr = parseInt(document.getElementById("input3").value);
+  var alm = parseInt(document.getElementById("input4").value);
+  var log_freq = parseInt(document.getElementById("input5").value);
+  var chart_freq = parseInt(document.getElementById("input6").value);
+
+  message = {
+        "device_id" : dev_id,
+        "alarm_high" : hi_tr,
+        "alarm_low" : lo_tr,
+        "alarm_set" : alm,
+        "graph_config" : chart_freq,
+        "logging_config" : log_freq
+  };
+
+  console.log(message);
+
+  stringified_message = JSON.stringify(message);
+
+  console.log(stringified_message);
+
+  sendMessage("ws_settings", stringified_message);
+  // Additional code to handle the cancellation of the form submission
+});
+
+</script>
 
 </body>
 </html>
